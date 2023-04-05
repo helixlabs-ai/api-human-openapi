f5751e06a9ff5f18de171edf5073b55edb6691e8
sheijl@bio-prodict.nl <sheijl@bio-prodict.nl>
2023-04-05 14:20:59 +0200

    Modified 3 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjYwNDM5/spectral.js?branch_slug=master&token=2c731b19-f02f-4484-a714-a628f62ed20d";
+export default { extends: ruleset };
